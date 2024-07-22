7cb8c750d56260197e21a14d526bb177235e7fcf
tonywarwick69 <duc.khang165@gmail.com>
2024-07-23 00:50:17 +0700

    First commit for NET MVC

---
+++ 1 RunGroupWebApp/RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace RunGroupWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+
+    }
+}
